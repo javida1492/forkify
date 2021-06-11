3a7918ff840cc1883312394b75c94edf046ce776
Andrew Javidfar <javida1492@gmail.com>
2021-06-10 23:56:28 -0400

    Updated API key.

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'f95a6597-44cf-4025-be21-d963287a79b5';
 export const MODAL_CLOSE_SEC = 2.5;
